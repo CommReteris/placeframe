a9a008a6964c3c46a1ea5b0585e3ea0aa7b3ce49
Tyler Hatch <tylershatch@gmail.com>
2026-03-19 15:14:42 +0000

    [code] Unity C# build scripts: BuildUtility, install, versioning
    - Add shared BuildUtility with staleness check, version injection, and build reporting
    - Flatten build artifact output, derive filenames from PlayerSettings.productName
    - Add install command with build caching and linux64 support
    - Unified versioning via build/versions.json for apps and packages
    - Update Unity package manifests and lock files

--- apps/AndroidMobile/Assets/Editor/BuildScript.cs
+++ apps/AndroidMobile/Assets/Editor/BuildScript.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Unity.Plastic.Newtonsoft.Json;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEditor.Build;
-using UnityEditor.Build.Reporting;
 using UnityEditor.XR.Management;
 using UnityEditor.XR.Management.Metadata;
 using UnityEngine;
@@ -25,84 +21,17 @@ namespace Placeframe.Client
             PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARM64;
             EditorUserBuildSettings.androidBuildSubtarget = MobileTextureSubtarget.ASTC;
 
-            string outputPath = "Build/AndroidMobile/AndroidMobile.apk";
-            Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
+            string sanitizedName = Regex.Replace(PlayerSettings.productName, @"[^a-zA-Z0-9._-]", "_");
+            string outputPath = $"Build/{sanitizedName}.apk";
+            Directory.CreateDirectory("Build");
 
-            BuildReport report = BuildPipeline.BuildPlayer(new BuildPlayerOptions
+            Placeframe.BuildUtility.BuildPlayer(new BuildPlayerOptions
             {
                 scenes = new[] { "Assets/Scenes/Main.unity" },
                 locationPathName = outputPath,
                 target = BuildTarget.Android,
                 targetGroup = BuildTargetGroup.Android,
             });
-
-            SerializableBuildReport serializableReport = new SerializableBuildReport();
-            serializableReport.result = report.summary.result.ToString();
-            (serializableReport.steps, serializableReport.messages) = BuildStepTree(new List<BuildStep>(report.steps), -1);
-            File.WriteAllText("Build/AndroidMobile/BuildReport.json", JsonConvert.SerializeObject(serializableReport, Formatting.Indented));
-
-            if (report.summary.result != BuildResult.Succeeded)
-            {
-                EditorApplication.Exit(1);
-            }
-        }
-
-        [Serializable]
-        public class SerializableBuildReport
-        {
-            public string result;
-            public List<Step> steps;
-            public List<Message> messages;
-        }
-
-        [Serializable]
-        public class Step
-        {
-            public string name;
-            public string duration;
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public List<Step> steps;
-        }
-
-        [Serializable]
-        public class Message
-        {
-            public string content;
-            public string type;
-            public string step;
-        }
-
-        private static (List<Step>, List<Message>) BuildStepTree(List<BuildStep> reportBuildSteps, int depth)
-        {
-            List<Step> steps = new List<Step>();
-            List<Message> messages = new List<Message>();
-
-            while (reportBuildSteps.Count > 0)
-            {
-                if (reportBuildSteps[0].depth <= depth) break;
-
-                BuildStep reportBuildStep = reportBuildSteps[0];
-                reportBuildSteps.RemoveAt(0);
-
-                messages.AddRange(reportBuildStep.messages
-                    .Select(m => new Message
-                    {
-                        content = m.content,
-                        type = m.type.ToString(),
-                        step = reportBuildStep.name
-                    }));
-
-                var (subSteps, subMessages) = BuildStepTree(reportBuildSteps, reportBuildStep.depth);
-                messages.AddRange(subMessages);
-
-                steps.Add(new Step
-                {
-                    name = reportBuildStep.name,
-                    duration = reportBuildStep.duration.TotalSeconds.ToString(),
-                    steps = subSteps.Count > 0 ? subSteps : null
-                });
-            }
-
-            return (steps, messages);
         }
     }
 }
